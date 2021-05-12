d2efba012a0a38c9e98603b9f920ee73834fd5f0
xuyuanyuan <17621368758@163.com>
2021-05-12 16:07:02 +0800

    feat

--- middleware/midd.js
+++ middleware/midd.js
@@ -2,11 +2,11 @@ const env = require("../env");
 let Murl = process.env.PC_URL
 export default function ({ isServer, req, redirect, route }) {
 
-  let mobileOrigin = Murl
+  let PcOrigin = Murl
   let isMobile = (ua) => {
     return !!ua.match(/AppleWebKit.*Mobile.*/)
   }
   let userAgent = req ? req.headers['user-agent'] : navigator.userAgent || ''
-  return isMobile(userAgent) ? redirect(mobileOrigin ) : ''
+  return isMobile(userAgent) ? '' : redirect(PcOrigin )
   // 使用redirect 重定向到外链需要加上前缀:http / https
 }
